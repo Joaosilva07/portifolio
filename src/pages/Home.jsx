99d4bffc5b81bff8194416109b33c8787f370d40
João nunes <126252207+Joaosilva07@users.noreply.github.com>
2025-07-14 19:58:56 -0300

    Skills section changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import { StarBackground } from "../components/StarBackground";
 import { Navbar } from "../components/navbar";
 import { HeroSection } from "../components/HeroSection";
 import { AboutSection } from "../components/AboutSection";
+import { SkillsSection } from "../components/Skills";
 
 
 export const Home = () => {
@@ -20,6 +21,7 @@ export const Home = () => {
        <main>
         <HeroSection />
         <AboutSection />
+        <SkillsSection />
        </main>
 
      {/*footer*/}
