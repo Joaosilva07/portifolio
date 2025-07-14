2f1a6154cdc7a1463c0d8a54676fe116eb5277a9
João nunes <126252207+Joaosilva07@users.noreply.github.com>
2025-07-14 20:19:52 -0300

    Contact section + toast implemented

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,8 @@ import { HeroSection } from "../components/HeroSection";
 import { AboutSection } from "../components/AboutSection";
 import { SkillsSection } from "../components/Skills";
 import { ProjectsSection } from "../components/ProjectsSection";
+import { ContactSection } from "../components/ContactSection";
+
 
 
 export const Home = () => {
@@ -24,6 +26,7 @@ export const Home = () => {
         <AboutSection />
         <SkillsSection />
         <ProjectsSection />
+        <ContactSection />
        </main>
 
      {/*footer*/}
