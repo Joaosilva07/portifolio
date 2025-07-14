2106b24dd16162aa42392f6dbd8f3a7c0af930de
João nunes <126252207+Joaosilva07@users.noreply.github.com>
2025-07-14 18:58:56 -0300

    First commits

---
+++ src/components/navbar.jsx
@@ -0,0 +1,14 @@
+import { href } from "react-router-dom"
+
+
+const navItems = [
+    {name: "Home", href: "#hero"},
+    {name: "About", href: "#about"},
+    {name: "Skills", href: "#Skills"},
+    {name: "Contact", href: "#Contact"},
+]
+
+export const Navbar = () => {
+
+
+}
